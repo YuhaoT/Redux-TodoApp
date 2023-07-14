f21c48292debf0fb1beefadfa781c622c3749dcf
YuhaoT <heyuhao97@gamil.com>
2023-07-14 16:10:36 -0400

    added express backend support

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import Card from "./components/Card";
-function App() {
-  return (
-    <>
-      <div className="container mx-auto flex items-center justify-center h-screen">
-        <Card />
-      </div>
-    </>
-  );
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // fetching the GET route from the Express server which matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+  render() {
+    return (
+      <>
+        <div className="container mx-auto flex items-center justify-center h-screen">
+          <Card />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
